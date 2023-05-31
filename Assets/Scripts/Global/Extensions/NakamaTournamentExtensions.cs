0d5d5561a0c1f7427494e222bb847454485baa4f
Anton Sukharev <sanjaplushka@gmail.com>
2023-05-31 20:49:32 +0500

    Missing files

--- Assets/Scripts/Global/Extensions/NakamaTournamentExtensions.cs
+++ Assets/Scripts/Global/Extensions/NakamaTournamentExtensions.cs
@@ -1,5 +1,25 @@
-﻿namespace Global.Extensions {
-    public class NakamaTournamentExtensions {
-        
+﻿using System;
+using Nakama;
+
+namespace Global.Extensions {
+    public static class NakamaTournamentExtensions {
+        public static bool IsActive(this IApiTournament tournament) {
+            var startTime = DateTimeOffset.Parse(tournament.StartTime).ToUnixTimeSeconds();
+            var endTime = startTime + tournament.Duration;
+            var nowTime = ((DateTimeOffset) DateTime.Now).ToUnixTimeSeconds();
+
+            var whenEnded = endTime - nowTime;
+            if (whenEnded < 0) return false;
+
+            return true;
+        }
+
+        public static long GetRemainingTime(this IApiTournament tournament) {
+            var startTime = DateTimeOffset.Parse(tournament.StartTime).ToUnixTimeSeconds();
+            var endTime = startTime + tournament.Duration;
+            var nowTime = ((DateTimeOffset) DateTime.Now).ToUnixTimeSeconds();
+
+            return endTime - nowTime;
+        }
     }
 }
